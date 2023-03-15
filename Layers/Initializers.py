4aede93decf59ec1bc2bb99a4667ba6eb250dd8b
HassanRady <hassan.khaled.rady@gmail.com>
2023-03-15 10:27:11 +0100

    Xavier done

--- Layers/Initializers.py
+++ Layers/Initializers.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Constant:
-    def __init__(self, value) -> None:
+    def __init__(self, value=0.1) -> None:
         self.value = value
     
     def initialize(self, weights_shape, fan_in, fan_out):
@@ -10,35 +10,26 @@ class Constant:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class UniformRandom:
     def __init__(self) -> None:
         pass
     
     def initialize(self, weights_shape, fan_in, fan_out):
-        weights = np.random.rand(weights_shape)
+        weights = np.random.rand(*weights_shape)
         return weights
+
+
 
 class Xavier:
     def __init__(self) -> None:
         pass
     
     def initialize(self, weights_shape, fan_in, fan_out):
-        return 0
+        sigma = np.sqrt( (2) / (fan_in + fan_out) )
+        weights = np.random.normal(0, sigma, size=weights_shape)
+        return weights
+
+
 
 class He:
     def __init__(self) -> None:
