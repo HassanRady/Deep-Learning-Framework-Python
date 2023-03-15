dec394f9a8376cf36b5b3ca7adf1bc7f84cc3d0a
HassanRady <hassan.khaled.rady@gmail.com>
2023-03-15 09:38:10 +0100

    init

---
+++ Layers/Initializers.py
@@ -0,0 +1,44 @@
+class Constant:
+    def __init__(self, value) -> None:
+        self.value = value
+    
+    def initialize(self, weights_shape, fan_in, fan_out):
+        return 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class UniformRandom:
+    def __init__(self) -> None:
+        pass
+    
+    def initialize(self, weights_shape, fan_in, fan_out):
+        return 0
+
+class Xavier:
+    def __init__(self) -> None:
+        pass
+    
+    def initialize(self, weights_shape, fan_in, fan_out):
+        return 0
+
+class He:
+    def __init__(self) -> None:
+        pass
+    
+    def initialize(self, weights_shape, fan_in, fan_out):
+        return 0
