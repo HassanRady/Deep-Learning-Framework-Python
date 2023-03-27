65dd40de4679932559e7fc582713d20b3c765d49
HassanRady <hassan.khaled.rady@gmail.com>
2023-03-27 16:09:51 +0200

    forward implementation done

--- Layers/Conv.py
+++ Layers/Conv.py
@@ -1,8 +1,16 @@
-from Layers.Base import BaseLayer
-import numpy as np
-from scipy import signal
-
 from logger import get_file_logger
+import numpy as np
+from Layers.Base import BaseLayer
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+
 _logger = get_file_logger(__name__)
 
 
@@ -11,7 +19,7 @@ class Conv(BaseLayer):
         super().__init__()
         self.trainable = True
 
-        self.stride_shape = self.to_tuple(stride_shape)
+        self.stride_shape = stride_shape
         self.convolution_shape = convolution_shape
 
         self.input_channels = self.convolution_shape[0]
@@ -23,18 +31,11 @@ class Conv(BaseLayer):
         self.weights = np.random.randn(*self.weight_shape)
         self.bias = np.random.randn(self.output_channels, 1)
 
-
     def initialize(self, weights_initializer, bias_initializer):
         self.weights = weights_initializer.initialize(self.weight_shape, np.prod(self.convolution_shape),
                                                       np.prod(self.convolution_shape[1:]) * self.output_channels)
         self.bias = bias_initializer.initialize(
             self.output_channels, self.input_channels, self.output_channels)
-
-    def to_tuple(self, obj):
-        if obj is tuple:
-            return obj
-        else:
-            return tuple(obj)
 
     @property
     def optimizer(self):
@@ -52,40 +53,22 @@ class Conv(BaseLayer):
     def gradient_bias(self):
         return self._gradient_bias
 
+    def get_shape_after_conv(self, dim_size, kernel_size, pad, stride) -> int:
+        (start_pad, end_pad) = pad
+        return 1 + (dim_size - kernel_size + start_pad + end_pad)//stride
 
-    def isValid(self, stride):
-        if stride[0] != stride[1]:
-            return True
-        else:
-            return False
+    def get_pad_size(self, kernel_size):
+        start_pad = (kernel_size - 1)//2
+        end_pad = kernel_size - start_pad - 1
+        return (start_pad, end_pad)
 
-    def isSame(self, stride):
-        if stride[0] == stride[1]:
-            return True
-        else:
-            return False
-        
-    def get_padding_type(self, stride):
-        if self.isValid(stride):
-            return "valid"
-        elif self.isSame(stride):
-            return "same"
-
-
-    def get_shape_after_conv(self, x, f, p=0, s=1) -> int:
-        return 1 + (x - f + 2*p)//s
-    
     def pad_img(self, img, dim1, dim2):
-        return np.pad(img, ((0, 0), (dim1, dim1), (dim2, dim2)), mode="constant")
+        (start_pad_dim1, end_pad_dim1) = dim1
+        (start_pad_dim2, end_pad_dim2) = dim2
+        return np.pad(img, ((0, 0), (start_pad_dim1, end_pad_dim1), (start_pad_dim2, end_pad_dim2)), mode="constant")
 
-    def pad_1d(self, x, start, end):
-        return np.pad(x, (start, end), mode="constant")
-
-    def get_num_of_pad_needed_to_same_img(self, dim_len, kernel_size, stride):
-        return (dim_len*(stride - 1) + kernel_size - stride)//2
-    
     def convolve(self, slice, kernel, bias):
-        return np.sum(slice * kernel) + bias 
+        return np.sum(slice * kernel) + bias
 
     def get_dims_for_output_img(self, input_tensor):
         self.batch_size = input_tensor.shape[0]
@@ -100,13 +83,15 @@ class Conv(BaseLayer):
 
         return input_size_dim1, input_size_dim2, kernel_size_dim1, kernel_size_dim2, stride_size_dim1, stride_size_dim2
 
-    def get_output_shape_for_img(self,input_size_dim1, input_size_dim2, kernel_size_dim1, kernel_size_dim2, stride_size_dim1, stride_size_dim2, pad_size_dim1, pad_size_dim2):
-        
-        output_dim1 = self.get_shape_after_conv(input_size_dim1, kernel_size_dim1, pad_size_dim1, stride_size_dim1)
-        output_dim2 = self.get_shape_after_conv(input_size_dim2, kernel_size_dim2, pad_size_dim2, stride_size_dim2)
+    def get_output_shape_for_img(self, input_size_dim1, input_size_dim2, kernel_size_dim1, kernel_size_dim2, stride_size_dim1, stride_size_dim2, pad_size_dim1, pad_size_dim2):
+
+        output_dim1 = self.get_shape_after_conv(
+            input_size_dim1, kernel_size_dim1, pad_size_dim1, stride_size_dim1)
+        output_dim2 = self.get_shape_after_conv(
+            input_size_dim2, kernel_size_dim2, pad_size_dim2, stride_size_dim2)
 
         return (self.batch_size, self.output_channels, output_dim1, output_dim2)
-    
+
     def get_slice(self, image, output_dim1, output_dim2, stride_size_dim1, stride_size_dim2, kernel_size_dim1, kernel_size_dim2):
         for i in range(output_dim1):
             for j in range(output_dim2):
@@ -117,30 +102,41 @@ class Conv(BaseLayer):
                 slice = image[:, start_dim1:end_dim1, start_dim2:end_dim2]
                 yield slice, i, j
 
-
     def forward(self, input_tensor: np.array):  # input shape BATCHxCHANNELSxHIGHTxWIDTH
-        input_size_dim1, input_size_dim2, kernel_size_dim1, kernel_size_dim2, stride_size_dim1, stride_size_dim2 = self.get_dims_for_output_img(input_tensor)
-        pad_size_dim1 = self.get_num_of_pad_needed_to_same_img(input_size_dim1, kernel_size_dim1, stride_size_dim1)
-        pad_size_dim2 = self.get_num_of_pad_needed_to_same_img(input_size_dim2, kernel_size_dim2, stride_size_dim2)
-        output_shape = self.get_output_shape_for_img(input_size_dim1, input_size_dim2, kernel_size_dim1, kernel_size_dim2, stride_size_dim1, stride_size_dim2, pad_size_dim1, pad_size_dim2)
+        input_size_dim1, input_size_dim2, kernel_size_dim1, kernel_size_dim2, stride_size_dim1, stride_size_dim2 = self.get_dims_for_output_img(
+            input_tensor)
+        pad_size_dim1 = self.get_pad_size(kernel_size_dim1, )
+        pad_size_dim2 = self.get_pad_size(kernel_size_dim2, )
+        output_shape = self.get_output_shape_for_img(
+            input_size_dim1, input_size_dim2, kernel_size_dim1, kernel_size_dim2, stride_size_dim1, stride_size_dim2, pad_size_dim1, pad_size_dim2)
         (_, _, output_dim1, output_dim2) = output_shape
 
         self.forward_output = np.zeros(output_shape)
 
         for n in range(self.batch_size):
             one_sample = input_tensor[n]
-            one_sample_padded = self.pad_img(one_sample, pad_size_dim1, pad_size_dim2)
+            one_sample_padded = self.pad_img(
+                one_sample, pad_size_dim1, pad_size_dim2)
 
             for out_channel in range(self.output_channels):
                 kernel = self.weights[out_channel]
                 bias = self.bias[out_channel]
 
                 for slice, i, j in self.get_slice(one_sample_padded, output_dim1, output_dim2, stride_size_dim1, stride_size_dim2, kernel_size_dim1, kernel_size_dim2):
-                    self.forward_output[n, out_channel, i, j] = self.convolve(slice, kernel, bias)
-                
-        _logger.debug(self.forward_output.shape)        
+                    self.forward_output[n, out_channel, i,
+                                        j] = self.convolve(slice, kernel, bias)
+
+        _logger.debug(self.forward_output.shape)
         return self.forward_output
 
     def backward(self, error_tensor):
         return error_tensor
 
+
+if __name__ == "__main__":
+
+    from NeuralNetworkTests import TestConv
+
+    tests = TestConv()
+    tests.setUp()
+    tests.test_forward_size_stride_uneven_image()
