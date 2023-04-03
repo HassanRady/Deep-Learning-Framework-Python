eb2cd7989d8ba5d1e6242132fbf323e1ca7bb594
HassanRady <hassan.khaled.rady@gmail.com>
2023-04-03 17:15:06 +0200

    adding main file for classification task

---
+++ main.py
@@ -0,0 +1,10 @@
+from Layers.Initializers import Xavier, He, UniformRandom, Constant
+from Layers.Conv import Conv
+from Layers.Pooling import Pooling
+from Layers.Flatten import Flatten
+from Layers.FullyConnected import FullyConnected
+from Layers.ReLU import ReLU
+from Layers.SoftMax import SoftMax
+from Optimization.Loss import CrossEntropyLoss
+from Optimization.Optimizers import Adam, SgdWithMomentum, Sgd
+
