a5cfd2acef81a153adb1a409141e93d4b562a87c
HassanRady <hassan.khaled.rady@gmail.com>
2023-04-05 09:51:41 +0200

    making conv2d more standard

--- Layers/Conv.py
+++ Layers/Conv.py
@@ -14,7 +14,7 @@ sys.path.insert(0, parentdir)
 _logger = get_file_logger(__name__)
 
 
-class Conv(BaseLayer):
+class Conv2d(BaseLayer):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
         super().__init__()
         self.trainable = True
@@ -26,10 +26,6 @@ class Conv(BaseLayer):
         self.stride_size_dim1 = self.stride_shape[0]
         self.stride_size_dim2 = self.stride_shape[1]
 
-
-
-        self.convolution_shape = convolution_shape
-
         self.in_channels = in_channels
         self.out_channels = out_channels
 
@@ -40,49 +36,30 @@ class Conv(BaseLayer):
         self.kernel_size_dim2 = self.kernel_size[1]
         self.is_reduction_kernel = self.kernel_size == (1, 1)
 
-        self.pad_size_dim1 = self.get_pad_size(self.kernel_size_dim1)
-        self.pad_size_dim2 = self.get_pad_size(self.kernel_size_dim2)
+        self.check_padding_type(padding)
 
         self.weight_shape = (
             self.out_channels, self.in_channels, *self.kernel_size)
         self.weights = np.random.randn(*self.weight_shape)
         self.bias = np.random.randn(self.out_channels, 1)
-
-
-
-    def __init__(self, stride_shape: int or tuple, convolution_shape: list, num_kernels: int):
-        super().__init__()
-        self.trainable = True
-        self._optimizer = None
-
-        self.stride_shape = stride_shape
-        self.stride_size_dim1 = self.stride_shape[0]
-        self.stride_size_dim2 = self.stride_shape[1]
-
-        self.convolution_shape = convolution_shape
-
-        self.in_channels = self.convolution_shape[0]
-        self.out_channels = num_kernels
-        self.kernel_size = self.convolution_shape[1:]
-        self.kernel_size_dim1 = self.kernel_size[0]
-        self.kernel_size_dim2 = self.kernel_size[1]
-        self.is_reduction_kernel = self.kernel_size == (1, 1)
-
-        self.pad_size_dim1 = self.get_pad_size(self.kernel_size_dim1)
-        self.pad_size_dim2 = self.get_pad_size(self.kernel_size_dim2)
-
-        self.weight_shape = (
-            self.out_channels, self.in_channels, *self.kernel_size)
-        self.weights = np.random.randn(*self.weight_shape)
-        self.bias = np.random.randn(self.out_channels, 1)
-
 
     def to_tuple(self, int_value):
         return (int_value, int_value)
 
+    def check_padding_type(self, padding):
+        if padding == "same":
+            self.pad_size_dim1 = self.get_pad_size_same(self.kernel_size_dim1)
+            self.pad_size_dim2 = self.get_pad_size_same(self.kernel_size_dim2)
+        elif padding == "valid":
+            self.pad_size_dim1 = (0, 0)
+            self.pad_size_dim2 = (0, 0)
+        elif isinstance(padding, int):
+            self.pad_size_dim1 = (padding, padding)
+            self.pad_size_dim2 = (padding, padding)
+
     def initialize(self, weights_initializer, bias_initializer):
-        self.weights = weights_initializer.initialize(self.weight_shape, np.prod(self.convolution_shape),
-                                                      np.prod(self.convolution_shape[1:]) * self.out_channels)
+        self.weights = weights_initializer.initialize(self.weight_shape, self.in_channels * self.kernel_size_dim1 * self.kernel_size_dim2,
+                                                      self.kernel_size_dim1 * self.kernel_size_dim2 * self.out_channels)
         self.bias = bias_initializer.initialize(
             self.out_channels, self.in_channels, self.out_channels)
 
@@ -106,7 +83,7 @@ class Conv(BaseLayer):
         (start_pad, end_pad) = pad
         return 1 + (dim_size - kernel_size + start_pad + end_pad)//stride
 
-    def get_pad_size(self, kernel_size):
+    def get_pad_size_same(self, kernel_size):
         # if its an odd kernel
         if kernel_size % 2 == 1:
             start_pad = (kernel_size - 1)//2
@@ -120,7 +97,7 @@ class Conv(BaseLayer):
         (start_pad_dim1, end_pad_dim1) = dim1
         (start_pad_dim2, end_pad_dim2) = dim2
         return np.pad(img, ((0, 0), (0, 0), (start_pad_dim1, end_pad_dim1), (start_pad_dim2, end_pad_dim2)), mode="constant")
-    
+
     def remove_pad(self, img):
         (start_pad_dim1, end_pad_dim1) = self.pad_size_dim1
         (start_pad_dim2, end_pad_dim2) = self.pad_size_dim2
@@ -152,15 +129,13 @@ class Conv(BaseLayer):
 
     def forward(self, input_tensor: np.array):  # input shape BATCHxCHANNELSxHIGHTxWIDTH
         self.input_tensor = input_tensor
-
-        self.input_tensor_padded = self.pad_img(input_tensor, self.pad_size_dim1, self.pad_size_dim2)
-
         (self.batch_size, _, input_size_dim1, input_size_dim2) = input_tensor.shape
-        
+        self.input_tensor_padded = self.pad_img(
+            input_tensor, self.pad_size_dim1, self.pad_size_dim2)
+
         self.forward_output_shape = self.get_output_shape_for_img(
             input_size_dim1, input_size_dim2)
         (_, _, output_dim1, output_dim2) = self.forward_output_shape
-
         self.forward_output = np.zeros(self.forward_output_shape)
 
         for n in range(self.batch_size):
@@ -193,14 +168,18 @@ class Conv(BaseLayer):
                     start_dim2 = j * self.stride_size_dim2
                     end_dim2 = j * self.stride_size_dim2 + self.kernel_size_dim2
 
-                    self._gradient_weights[out_channel] += slice * error_tensor[n, out_channel, i, j]
-                    self._gradient_bias[out_channel] += error_tensor[n, out_channel, i, j]
-                    gradient_input[n, :, start_dim1:end_dim1, start_dim2:end_dim2] += error_tensor[n, out_channel, i, j] * self.weights[out_channel]
-            
+                    self._gradient_weights[out_channel] += slice * \
+                        error_tensor[n, out_channel, i, j]
+                    self._gradient_bias[out_channel] += error_tensor[n,
+                                                                     out_channel, i, j]
+                    gradient_input[n, :, start_dim1:end_dim1, start_dim2:end_dim2] += error_tensor[n,
+                                                                                                   out_channel, i, j] * self.weights[out_channel]
+
             output[n] = self.remove_pad(gradient_input[n])
-                    
+
         if self.optimizer:
-            self.weights = self.optimizer.calculate_update(self.weights, self.gradient_weights)
+            self.weights = self.optimizer.calculate_update(
+                self.weights, self.gradient_weights)
 
         return output
 
