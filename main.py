ed584782e17c92b92f01edb9319058bce815f4f5
HassanRady <hassan.khaled.rady@gmail.com>
2023-04-15 15:15:33 +0200

    updating main

--- main.py
+++ main.py
@@ -9,24 +9,23 @@ from Layers.SoftMax import SoftMax
 from Optimization.Loss import CrossEntropyLoss
 from Optimization.Optimizers import Adam, SgdWithMomentum, Sgd
 
-from Train import Trainer
-from Layers.Helpers import DigitData
+from Model import Model
 
+import numpy as np
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
 input_folder_path = "Data/"
-train_df = pd.read_csv(input_folder_path+"train.csv")
+train_df = pd.read_csv(input_folder_path+"train.csv")[:100]
 test_df = pd.read_csv(input_folder_path+"test.csv")
 
 train_labels = train_df['label'].values
-train_images = (train_df.iloc[:,1:].values).astype('float32')
-test_images = (test_df.iloc[:,:].values).astype('float32')
+train_images = (train_df.iloc[:, 1:].values).astype('float32')
+test_images = (test_df.iloc[:, :].values).astype('float32')
 
-#Training and Validation Split
 train_images, val_images, train_labels, val_labels = train_test_split(train_images, train_labels,
-                                                                     stratify=train_labels, random_state=123,
-                                                                     test_size=0.20)
+                                                                      stratify=train_labels, random_state=123,
+                                                                      test_size=0.20)
 train_images = train_images.reshape(train_images.shape[0], 28, 28)
 val_images = val_images.reshape(val_images.shape[0], 28, 28)
 test_images = test_images.reshape(test_images.shape[0], 28, 28)
@@ -35,8 +34,11 @@ train_images = train_images/255.0
 val_images = val_images/255.0
 test_images = test_images/255.0
 
-    
+classes = 10
 
+train_images = np.expand_dims(train_images, axis=1)
+train_labels = train_labels.reshape(-1)
+train_labels = np.eye(classes)[train_labels]
 
 model = [
     Conv2d(in_channels=1, out_channels=32,
@@ -46,19 +48,19 @@ model = [
 
     Conv2d(in_channels=32, out_channels=64,
            kernel_size=3, stride=1, padding='same'),
-    BatchNorm2d(64),
+        BatchNorm2d(64),
     ReLU(),
     MaxPool2d(kernel_size=2, stride=2),
 
-    Conv2d(in_channels=64, out_channels=128,
+    Conv2d(in_channels=64, out_channels=64,
            kernel_size=3, stride=1, padding='same'),
-    BatchNorm2d(128),
+        BatchNorm2d(64),
     ReLU(),
     MaxPool2d(kernel_size=2, stride=2),
 
 
     Flatten(),
-    Linear(in_features=128*7*7, out_features=128),
+    Linear(in_features=64*7*7, out_features=128),
     ReLU(),
     Linear(128, 64),
     ReLU(),
@@ -66,12 +68,15 @@ model = [
     SoftMax(),
 ]
 
-# x, y = DigitData(200).next()
+batch_size = 16
+model = Model(model)
+model.compile(Adam(5e-3, 0.98, 0.999), CrossEntropyLoss(), batch_size)
 
-# trainer = Trainer(model, Adam(5e-3, 0.98, 0.999),
-#                   He(),
-#                   Constant(0.1))
+epochs = 25
+for i in range(1, epochs+1):
+    print(f"Epoch {i}:")
 
-# trainer.loss_layer = CrossEntropyLoss()
+    train_loss = model.train(train_images, train_labels)
+#     val_loss = evaluator.eval_epoch()
 
-# trainer.fit(200, [[x, y]])
+    print(f"  Train Loss: {train_loss}")
