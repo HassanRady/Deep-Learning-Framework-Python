1a7c2ab022b2c49d4e9e7006d4eac1cc066aef20
HassanRady <hassan.khaled.rady@gmail.com>
2023-04-15 15:15:21 +0200

    making model to train and validate

--- Model.py
+++ Model.py
@@ -1,9 +1,73 @@
 import copy
+from Layers.Base import BaseLayer
+from logger import get_file_logger
+
+import numpy as np
+
+_logger = get_file_logger(__name__)
 
 class Model(object):
-    def __init__(self) -> None:
-        self.model = None
-        self.loss_layer = None
+    def __init__(self, model=None) -> None:
+        self.model = []
+
+        if isinstance(model, list):
+            self.model = model
+
+    def train_step(self, x, y):
+        output = self.forward(x)
+        loss = self.loss.forward(output, y)
+        self.backward(y)
+        _logger.debug(f"loss : {loss}")
+        return loss
+    
+    def val_step(self, x, y):
+        output = self.forward(x)
+        loss = self.loss_layer.forward(output, y)
+        return loss
+    
+    def train_epoch(self):
+        loss = 0.0
+        for x_batch, y_batch in self.batcher(self.x_train, self.y_train):
+            loss += self.train_step(x_batch, y_batch)
+        epoch_loss = loss/len(self.x_train)
+        return epoch_loss
+
+    def eval_epoch(self):
+        loss = 0.0
+        for x_batch, y_batch in self.batcher(self.x_val, self.y_val):
+            loss += self.val_step(x_batch, y_batch)
+        epoch_loss = loss/len(self.x_val)
+        return epoch_loss
+
+    def batcher(self, x, y):
+        x = np.array_split(x, len(x)//self.batch_size)
+        y = np.array_split(y, len(y)//self.batch_size)
+        for x_batch, y_batch in zip(x, y):
+            yield x_batch, y_batch
+    
+    def train(self, x, y):
+        self.x_train = x
+        self.y_train = y
+        return self.train_epoch()
+
+    def eval(self, x, y,):
+        self.x_val = x
+        self.y_val = y
+        return self.eval_epoch()
+
+    def append_layer(self, layer: BaseLayer):
+        if isinstance(layer, BaseLayer):
+            self.model.append(layer)
+
+    def compile(self, optimizer, loss, batch_size):
+        self.batch_size = batch_size
+        self.loss = loss
+        self.set_optimizer(optimizer)
+
+    def set_optimizer(self, optimizer):
+        for layer in self.model:
+            if layer.trainable:
+                layer.optimizer = copy.deepcopy(optimizer)
 
     def forward(self, x):
         for layer in self.model:
@@ -12,23 +76,7 @@ class Model(object):
         return output
 
     def backward(self, y):
-        y = self.loss_layer.backward(y)
-
+        y = self.loss.backward(y)
         for layer in reversed(self.model):
             output = layer.backward(y)
             y = output
-
-    def append_layer(self, layer):
-        if layer.trainable:
-            layer.optimizer = copy.deepcopy(self.optimizer)
-            layer.initialize(*self.initializer)
-        self.model.append(layer)
-
-    def compile(self, optimizer, loss):
-        self.set_optimizer(optimizer)
-        self.loss_layer = loss
-
-    def set_optimizer(self, optimizer):
-        for layer in self.model:
-            if layer.trainable:
-                layer.optimizer = copy.deepcopy(optimizer)
