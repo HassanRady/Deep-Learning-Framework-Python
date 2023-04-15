62d7d547146a91fdbb4f485d7a07e6b15f3757ee
HassanRady <hassan.khaled.rady@gmail.com>
2023-04-15 14:57:38 +0200

    adding dropout test cases

--- NeuralNetworkTests.py
+++ NeuralNetworkTests.py
@@ -1078,6 +1078,63 @@ class TestBatchNorm2d(unittest.TestCase):
             self.assertLess(np.sum(np.power(output_tensor, 2)),
                             np.sum(np.power(new_output_tensor, 2)))
 
+class TestDropout(unittest.TestCase):
+    def setUp(self):
+        self.batch_size = 10000
+        self.input_size = 10
+        self.input_tensor = np.ones((self.batch_size, self.input_size))
+
+    def test_trainable(self):
+        layer = Dropout.Dropout(0.25)
+        self.assertFalse(layer.trainable)
+
+    def test_default_phase(self):
+        drop_layer = Dropout.Dropout(0.25)
+        self.assertFalse(drop_layer.testing_phase)
+
+    def test_forward_trainTime(self):
+        drop_layer = Dropout.Dropout(0.25)
+        output = drop_layer.forward(self.input_tensor)
+        self.assertEqual(np.max(output), 4)
+        self.assertEqual(np.min(output), 0)
+        sum_over_mean = np.sum(np.mean(output, axis=0))
+        self.assertAlmostEqual(sum_over_mean/self.input_size, 1., places=1)
+
+    def test_position_preservation(self):
+        drop_layer = Dropout.Dropout(0.5)
+        output = drop_layer.forward(self.input_tensor)
+        error_prev = drop_layer.backward(self.input_tensor)
+        np.testing.assert_almost_equal(np.where(output == 0.), np.where(error_prev == 0.))
+
+    def test_forward_testTime(self):
+        drop_layer = Dropout.Dropout(0.5)
+        drop_layer.testing_phase = True
+        output = drop_layer.forward(self.input_tensor)
+
+        self.assertEqual(np.max(output), 1.)
+        self.assertEqual(np.min(output), 1.)
+        sum_over_mean = np.sum(np.mean(output, axis=0))
+        self.assertEqual(sum_over_mean, 1. * self.input_size)
+
+    def test_backward(self):
+        drop_layer = Dropout.Dropout(0.5)
+        drop_layer.forward(self.input_tensor)
+        output = drop_layer.backward(self.input_tensor)
+        self.assertEqual(np.max(output), 2)
+        self.assertEqual(np.min(output), 0)
+
+    def test_gradient(self):
+        batch_size = 10
+        input_size = 10
+        input_tensor = np.ones((batch_size, input_size))
+        label_tensor = np.zeros([batch_size, input_size])
+        for i in range(batch_size):
+            label_tensor[i, np.random.randint(0, input_size)] = 1
+        layers = list()
+        layers.append(Dropout.Dropout(0.5))
+        layers.append(L2Loss())
+        difference = Helpers.gradient_check(layers, input_tensor, label_tensor, seed=1337)
+        self.assertLessEqual(np.sum(difference), 1e-5)
 
 class TestNeuralNetwork2(unittest.TestCase):
     plot = True
