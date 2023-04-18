45a5e23158bd5684f18d9156b6185555b6d36ca7
HassanRady <hassan.khaled.rady@gmail.com>
2023-04-18 09:30:47 +0200

    fixing eval in model

--- Model.py
+++ Model.py
@@ -17,31 +17,36 @@ class Model(object):
         output = self.forward(x)
         loss = self.loss.forward(output, y)
         self.backward(y)
-        _logger.debug(f"loss : {loss}")
         return loss
     
     def val_step(self, x, y):
         output = self.forward(x)
-        loss = self.loss_layer.forward(output, y)
+        loss = self.loss.forward(output, y)
         return loss
     
     def train_epoch(self):
-        loss = 0.0
+        running_preds = []
+        running_loss = 0.0
         for x_batch, y_batch in self.batcher(self.x_train, self.y_train):
-            loss += self.train_step(x_batch, y_batch)
-        epoch_loss = loss/len(self.x_train)
+            batch_loss, predictions = self.train_step(x_batch, y_batch)
+            
+            running_loss += batch_loss
+            running_preds.append(predictions)
+
+        epoch_loss = running_loss/self.data_len
         return epoch_loss
 
     def eval_epoch(self):
         loss = 0.0
         for x_batch, y_batch in self.batcher(self.x_val, self.y_val):
             loss += self.val_step(x_batch, y_batch)
-        epoch_loss = loss/len(self.x_val)
+        epoch_loss = loss/self.data_len
         return epoch_loss
 
     def batcher(self, x, y):
         x = np.array_split(x, len(x)//self.batch_size)
         y = np.array_split(y, len(y)//self.batch_size)
+        self.data_len = len(x)
         for x_batch, y_batch in zip(x, y):
             yield x_batch, y_batch
     
@@ -50,19 +55,34 @@ class Model(object):
         self.y_train = y
         return self.train_epoch()
 
-    def eval(self, x, y,):
+    def eval(self, x, y, epochs):
         self.x_val = x
         self.y_val = y
-        return self.eval_epoch()
+
+        val_losses = []
+        val_preds = []
+
+        for i in range(1, epochs+1):
+            print(f"Epoch {i}: \n")
+
+            loss, preds = self.eval_epoch()
+
+            val_losses.append(loss)
+            val_preds.append(preds)
+
+        # metric = calc_metric(val_preds)
+        return {"loss": val_losses, "preds": val_preds}
 
     def append_layer(self, layer: BaseLayer):
         if isinstance(layer, BaseLayer):
             self.model.append(layer)
 
-    def compile(self, optimizer, loss, batch_size):
+    def compile(self, optimizer, loss, batch_size, metrics):
         self.batch_size = batch_size
         self.loss = loss
         self.set_optimizer(optimizer)
+
+
 
     def set_optimizer(self, optimizer):
         for layer in self.model:
