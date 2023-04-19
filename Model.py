2d91d13fb8eab67310f54e452a0f96f6469fe4eb
HassanRady <hassan.khaled.rady@gmail.com>
2023-04-19 10:13:18 +0200

    calculating accuracy for train & val

--- Model.py
+++ Model.py
@@ -4,7 +4,8 @@ from logger import get_file_logger
 
 import numpy as np
 
-_logger = get_file_logger(__name__)
+_logger = get_file_logger(__name__, 'debug')
+
 
 
 class Model(object):
@@ -17,6 +18,7 @@ class Model(object):
             self.model = model
 
     def train_step(self, x, y):
+        _logger.debug("test")
         output = self.forward(x)
         loss = self.loss.forward(output, y)
         self.backward(y)
@@ -31,22 +33,43 @@ class Model(object):
         running_preds = []
         running_loss = 0.0
         for x_batch, y_batch in self.batcher(self.x_train, self.y_train):
-            batch_loss, predictions = self.train_step(x_batch, y_batch)
+            batch_loss, preds = self.train_step(x_batch, y_batch)
 
             running_loss += batch_loss
-            running_preds.append(predictions)
+            running_preds.append(preds)
 
         epoch_loss = running_loss/self.data_len
+
+        running_preds = np.array(running_preds)
+        running_preds = running_preds.reshape(running_preds.shape[0]*running_preds.shape[1], running_preds.shape[2])
+
+        self.calc_metrics(running_preds, self.y_train)
+
+        print(f"Train loss: {epoch_loss:.2f}")
+        for metric, metric_output in self.metrics_output.items():
+            print(f"Train {metric}: {metric_output}")
+
         return epoch_loss, running_preds
 
     def eval_epoch(self):
         running_preds = []
         running_loss = 0.0
         for x_batch, y_batch in self.batcher(self.x_val, self.y_val):
-            batch_loss, predictions = self.val_step(x_batch, y_batch)
+            batch_loss, preds = self.val_step(x_batch, y_batch)
 
             running_loss += batch_loss
-            running_preds.append(predictions)
+            running_preds.append(preds)
+
+        epoch_loss = running_loss/self.data_len
+
+        running_preds = np.array(running_preds)
+        running_preds = running_preds.reshape(running_preds.shape[0]*running_preds.shape[1], running_preds.shape[2])
+
+        self.calc_metrics(running_preds, self.y_val)
+
+        print(f"Val loss: {epoch_loss:.2f}")
+        for metric, metric_output in self.metrics_output.items():
+            print(f"Val {metric}: {metric_output}")
 
         epoch_loss = batch_loss/self.data_len
         return epoch_loss, running_preds
@@ -70,7 +93,7 @@ class Model(object):
         val_preds = []
 
         for i in range(1, epochs + 1):
-            print(f"EPOCH {i}: ")
+            print(f"Epoch {i}: ")
 
             train_loss, train_pred = self.train_epoch()
             val_loss, val_pred = self.eval_epoch()
@@ -80,13 +103,13 @@ class Model(object):
             val_losses.append(val_loss)
             val_preds.append(val_pred)
 
-            print(f"Train Loss: {train_loss:.2f}")
-            print(f"Val Loss: {val_loss:.2f} \n")
+            print()
 
         self.train_output['loss'] = train_losses
         self.train_output['predictions'] = train_preds
         self.eval_output['loss'] = val_losses
         self.eval_output['predictions'] = val_preds
+
         return self.train_output, self.eval_output
 
     def train(self, x, y, epochs):
@@ -131,10 +154,23 @@ class Model(object):
         if isinstance(layer, BaseLayer):
             self.model.append(layer)
 
-    def compile(self, optimizer, loss, batch_size):
+    def compile(self, optimizer, loss, batch_size, metrics: list):
         self.batch_size = batch_size
         self.loss = loss
         self.set_optimizer(optimizer)
+        self.metrics = metrics
+
+    def calc_metrics(self, preds, labels):
+        self.metrics_output = {}
+        for metric in self.metrics:
+            if metric == "accuracy":
+                self.metrics_output['accuracy'] = self.calc_accuracy(preds, labels)
+
+    def calc_accuracy(self, preds, labels):
+        preds = np.argmax(preds, axis=1)
+        labels = np.argmax(labels, axis=1)
+        accuracy = np.mean(preds == labels)
+        return accuracy
 
     def set_optimizer(self, optimizer):
         for layer in self.model:
