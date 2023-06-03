4063f4bdf4b95ce206b1f8541fae46799f5a0328
HassanRady <hassan.khaled.rady@gmail.com>
2023-06-03 11:31:35 +0200

    refactoring

--- DLstorm/Layers/BatchNormalization.py
+++ DLstorm/Layers/BatchNormalization.py
@@ -28,9 +28,6 @@ class BatchNorm2d(BaseLayer):
         self.mean = np.zeros(self.num_features, dtype=np.float64)
         self.variance = np.ones(self.num_features)
 
-        self.training = True
-        self.input_tensor = None
-
     @property
     def optimizer(self):
         return self._optimizer
@@ -55,17 +52,6 @@ class BatchNorm2d(BaseLayer):
     def gradient_bias(self, value):
         self._gradient_bias = value
 
-    # def reshape_tensor_for_input(self, tensor):
-    #     return np.concatenate(tensor.reshape(tensor.shape[0], tensor.shape[1], tensor.shape[2] * tensor.shape[3]), axis=1).T
-
-    # def reshape_tensor_for_output(self, tensor):
-    #     return np.transpose(
-    #         tensor.reshape(self.input_tensor.shape[0], self.input_tensor.shape[2] * self.input_tensor.shape[3],
-    #                        self.input_tensor.shape[1]), (0, 2, 1)).reshape(self.input_tensor.shape[0],
-    #                                                                        self.input_tensor.shape[1],
-    #                                                                        self.input_tensor.shape[2],
-    #                                                                        self.input_tensor.shape[3])
-
     def normalize_train(self, tensor):
         batch_mean = np.mean(tensor, axis=(0, 2, 3), keepdims=False)
         batch_variance = np.var(tensor, axis=(0, 2, 3), keepdims=False)
@@ -76,7 +62,7 @@ class BatchNorm2d(BaseLayer):
             (1 - self.momentum) * self.mean
         self.variance = self.momentum * batch_variance * n / (n - 1) + \
             (1 - self.momentum) * self.variance
-        
+
         self.input_tensor_normalized = (
             tensor - batch_mean[None, :, None, None])/batch_std[None, :, None, None]
         input_batch_normalized = self.weight[None, :, None, None] * \
@@ -85,12 +71,12 @@ class BatchNorm2d(BaseLayer):
 
     def normalize_test(self, tensor):
         input_tensor_normalized = (
-            tensor - self.mean)/np.sqrt(self.variance + self.eps)
-        input_normalized = self.weight[None, :, None, None] * input_tensor_normalized + self.bias[None, :, None, None]
+            tensor - self.mean[None, :, None, None])/np.sqrt(self.variance[None, :, None, None] + self.eps)
+        input_normalized = self.weight[None, :, None, None] * \
+            input_tensor_normalized + self.bias[None, :, None, None]
         return input_normalized
 
     def forward(self, input_tensor):  # input shape BATCHxCHANNELSxHEIGHTxWIDTH
-        self.input_tensor = input_tensor
         self.batch_size = input_tensor.shape[0]
 
         if self.training:
@@ -102,19 +88,20 @@ class BatchNorm2d(BaseLayer):
 
     def backward(self, error_tensor):
 
-        gradient_gamma = np.sum(
+        gradient_weight = np.sum(
             error_tensor * self.input_tensor_normalized, axis=(0, 2, 3))
-        gradient_beta = error_tensor.sum(axis=(0, 2, 3))
-        gradient_input_normalized = error_tensor * self.weight[None, :, None, None]
+        gradient_bias = error_tensor.sum(axis=(0, 2, 3))
+        gradient_input_normalized = error_tensor * \
+            self.weight[None, :, None, None]
 
         der_input_tensor = 1./(self.batch_size * np.sqrt(self.variance[None, :, None, None] + self.eps)) * (self.batch_size * gradient_input_normalized -
-                                                                                       gradient_input_normalized.sum(axis=0) -
-                                                                                       self.input_tensor_normalized * np.sum(gradient_input_normalized * self.input_tensor_normalized, axis=0))
+                                                                                                            gradient_input_normalized.sum(axis=0) -
+                                                                                                            self.input_tensor_normalized * np.sum(gradient_input_normalized * self.input_tensor_normalized, axis=0))
         if self.optimizer:
             self.weight = self.optimizer.calculate_update(
-                self.weight, gradient_gamma)
+                self.weight, gradient_weight)
             self.bias = self.optimizer.calculate_update(
-                self.bias, gradient_beta)
+                self.bias, gradient_bias)
 
         return der_input_tensor
 
