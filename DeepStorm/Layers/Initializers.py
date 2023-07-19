778ee3ed738c6591da5f40065d69c6540d63b0f8
HassanRady <hassan.khaled.rady@gmail.com>
2023-07-19 09:57:24 +0200

    cleaning

--- DeepStorm/Layers/Initializers.py
+++ DeepStorm/Layers/Initializers.py
@@ -2,10 +2,10 @@ import numpy as np
 
 
 class Constant:
-    def __init__(self, value=0.1) -> None:
+    def __init__(self, value=0.01) -> None:
         self.value = value
 
-    def initialize(self, weights_shape, fan_in, fan_out):
+    def initialize(self, weights_shape=None, fan_in=None, fan_out=None):
         return np.full((fan_in, fan_out), self.value)
 
 
