b91aab9b8c784ac28566c01d4734cc76b400f643
HassanRady <hassan.khaled.rady@gmail.com>
2023-08-02 13:14:08 +0200

    cleaning

--- DeepStorm/Layers/Base.py
+++ DeepStorm/Layers/Base.py
@@ -2,13 +2,10 @@ class BaseLayer:
     def __init__(self, ):
         self.trainable = False
         self.initializable = False
-        self.training = True
+        self.training = False
 
     def train(self):
         self.training = True
 
     def eval(self):
         self.training = False
-
-if __name__ == "__main__":
-    pass
